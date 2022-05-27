b142ee6116b0451d68c1551f45e5f09909252615
Chao Chun Wang <i22062@hotmail.com>
2022-05-27 16:37:58 +0800

    Add member route

--- server.js
+++ server.js
@@ -1,29 +1,29 @@
 const express = require("express");
 const cors = require("cors");
 
-
-
-
-
 const app = express();
 
-
-// parse application/x-www-form-urlencoded 
-app.use(express.urlencoded({ extended: false })); 
-// parse application/json 
+// parse application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: false }));
+// parse application/json
 app.use(express.json());
 
-const whitelist = [undefined, 'http://localhost:5500', 'http://127.0.0.1:5500', 'http://localhost:3000']
+const whitelist = [
+  undefined,
+  "http://localhost:5500",
+  "http://127.0.0.1:5500",
+  "http://localhost:3000",
+];
 const corsOptions = {
-    credentials: true,
-    origin: function(origin, cb){
-        //console.log(origin);
-        if(whitelist.indexOf(origin) !== -1){
-            cb(null, true);
-        } else {
-            cb(null, false);
-        }
+  credentials: true,
+  origin: function (origin, cb) {
+    //console.log(origin);
+    if (whitelist.indexOf(origin) !== -1) {
+      cb(null, true);
+    } else {
+      cb(null, false);
     }
+  },
 };
 app.use(cors(corsOptions));
 
@@ -34,14 +34,15 @@ app.get("/", (req, res) => {
 
 // app.use('/img-uploads', require(__dirname+'/public/img-uploads/'))
 
-app.use('/items', require(__dirname+'/items'))
-app.use('/category', require(__dirname+'/category'))
-app.use('/membercenter', require(__dirname+'/membercenter'))
+app.use("/items", require(__dirname + "/items"));
+app.use("/category", require(__dirname + "/category"));
+app.use("/membercenter", require(__dirname + "/membercenter"));
+app.use("/member", require(__dirname + "/member"));
 //相當於const membercenter = require(__dirname+'/membercenter')//再把變數放上面
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // set port, listen for requests
-app.listen(3002, function(){
-    console.log("successfully connect")
-})
+app.listen(3002, function () {
+  console.log("successfully connect");
+});
